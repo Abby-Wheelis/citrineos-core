1f91ba5934832a2e30cb75c2a87f7527f1be0393
Abby Wheelis <Abigail.Wheelis@nrel.gov>
2025-08-04 11:28:41 -0600

    copilot pt 2

--- 02_Util/src/util/directus.ts
+++ 02_Util/src/util/directus.ts
@@ -136,7 +136,7 @@ export class DirectusUtil implements ConfigStore {
     if (filePath) {
       formData.append('folder', filePath);
     }
-    formData.append('file', new Blob([content.buffer]), fileName);
+    formData.append('file', new Blob([new Uint8Array(content.buffer)]), fileName);
     try {
       const file = await this._client.request(uploadFiles(formData));
       return file['id'];
